8dba110f0af238b00118589f1844d91d18408492
Linus Jahn <lnj@kaidan.im>
2020-09-09 15:05:23 +0200

    Add reporting of IQ responses with QFutures

--- src/base/QXmppStream.h
+++ src/base/QXmppStream.h
@@ -36,6 +36,7 @@ class QDomElement;
 template<typename T>
 class QFuture;
 class QSslSocket;
+class QXmppIq;
 class QXmppPacket;
 class QXmppStanza;
 class QXmppStreamPrivate;
@@ -55,6 +56,10 @@ public:
 
     bool sendPacket(const QXmppStanza &);
     QFuture<QXmpp::PacketState> sendPacketAsync(const QXmppStanza &);
+
+    using IqResult = std::variant<QDomElement, QXmpp::PacketState>;
+    QFuture<IqResult> sendIq(const QXmppIq &);
+    void cancelOngoingIqs();
 
     void resetPacketCache();
 
@@ -104,6 +109,7 @@ private:
 
     void processData(const QString &data);
     void sendPacket(QXmppPacket &packet);
+    bool handleIqResponse(const QDomElement &);
 
     QXmppStreamPrivate *const d;
 };
